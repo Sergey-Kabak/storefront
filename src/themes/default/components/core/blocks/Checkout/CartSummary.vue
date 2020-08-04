2b526de8995fb3c6002384a9951ea07a24c22964
Rodion <asvrodya@gmail.com>
2020-08-05 02:49:24 +0300

    Add translate to Grand Total in checkout page

--- src/themes/default/components/core/blocks/Checkout/CartSummary.vue
+++ src/themes/default/components/core/blocks/Checkout/CartSummary.vue
@@ -17,7 +17,7 @@
 
         <div class="row pt20 pb20 pl30 pr55 weight-400 h3" v-for="(segment, index) in totals" :key="index" v-if="segment.code === 'grand_total'">
           <div class="col-xs">
-            {{ segment.title }}
+            {{ $t(segment.title) }}
           </div>
           <div class="col-xs align-right">
             {{ segment.value | price(storeView) }}
