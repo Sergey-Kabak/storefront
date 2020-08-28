e1fc61fae32ac4543d420bc5e0c594b5fa525593
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2020-08-28 11:24:28 +0300

    add desctop checkout header

---
+++ src/themes/default/components/core/PhoneInfo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="phone-tooltip">
+    <span class="phone-tooltip-title">{{ $t('Consultation by telephone') }}</span>
+    <a class="phone-tooltip-number" href="tel:+380674666111">+38 067 466 61 11</a>
+    <span class="phone-tooltip-date">Пн-Пт: 9:00-18:00; Сб-Вс: {{ $t('Weekend') }}</span>
+    <span class="phone-tooltip-description">{{ $t('Free of charge from landlines and mobile phones in Ukraine') }}</span>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.phone-tooltip {
+	&-title {
+		font-size: 18px;
+		line-height: 20px;
+		font-weight: 600;
+		color: #1A1919;
+		margin-bottom: 16px;
+	}
+	&-number {
+		width: fit-content;
+		color: #23BE20!important;
+		font-size: 15px;
+		margin-bottom: 16px;
+		text-decoration: none;
+
+		&:after {
+			content: none;
+		}
+	}
+
+	&-date {
+		font-size: 14px;
+		color: #5F5E5E;
+		margin-bottom: 12px;
+	}
+
+	&-description {
+		color: rgba(95, 94, 94, 0.6);
+	}
+}
+
+a.underline:after, a:not(.no-underline):hover:after {
+  content: none;
+}
+</style>
