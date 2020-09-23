a521149b91f9580bdb21e6f05fcfaf8cb1a51e57
unknown <kozitskiy.bo6dan@gmail.com>
2020-09-23 11:05:26 +0300

    Revert "remove free by ukraine"
    This reverts commit 11dcf4f6aea01a5e1bef0b7d932b70f171310aa8.

--- src/themes/default/components/core/PhoneInfo.vue
+++ src/themes/default/components/core/PhoneInfo.vue
@@ -4,7 +4,7 @@
 		<a class="phone-tooltip-number" href="tel:+380674666111">+38 073 090 87 07</a>
 		<a class="phone-tooltip-number" href="tel:+380970908707">+38 097 090 87 07</a>
 		<span class="phone-tooltip-date">Пн-Пт: 9:00-18:00; Сб-Вс: {{ $t('Weekend') }}</span>
-		<!--<span class="phone-tooltip-description">{{ $t('Free of charge from landlines and mobile phones in Ukraine') }}</span>-->
+		<span class="phone-tooltip-description">{{ $t('Free of charge from landlines and mobile phones in Ukraine') }}</span>
 	</div>
 </template>
 
