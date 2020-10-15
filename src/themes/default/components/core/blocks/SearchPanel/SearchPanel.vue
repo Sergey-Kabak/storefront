1edf932eed9eb631627d553f741414d591017461
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2020-10-15 16:08:24 +0300

    Объединенный запрос на вытягивание 18618: fix homepage autofocus
    fix homepage autofocus

--- src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
+++ src/themes/default/components/core/blocks/SearchPanel/SearchPanel.vue
@@ -29,7 +29,6 @@
           @input="makeSearch"
           class="search-panel-input"
           :placeholder="$t('find product or brand')"
-          autofocus="true"
           autocomplete="chrome-off"
         >
         <svg v-if="search" @click="resetSearch()" class="reset-icon" fill="#BDBDBD" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
