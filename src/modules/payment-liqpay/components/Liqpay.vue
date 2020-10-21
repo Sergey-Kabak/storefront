5940d442b5dc136f5c62f8527551d772782d9a83
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2020-10-21 17:00:47 +0300

    Объединенный запрос на вытягивание 18578: redirect to another liqpay account with marketplace sign
    redirect to another liqpay account with marketplace sign

--- src/modules/payment-liqpay/components/Liqpay.vue
+++ src/modules/payment-liqpay/components/Liqpay.vue
@@ -50,14 +50,14 @@ export default {
 			return Base64.stringify(
 				Utf8.parse(
 					JSON.stringify({
-						public_key: config.liqpay.public_key,
-						version: config.liqpay.version,
+						public_key: this.liqpayData.public_key,
+						version: '3',
 						action: 'pay',
-						currency: config.liqpay.currency,
+						currency: 'UAH',
 						amount: this.totalPrice,
             order_id: this.incrementId,
             incrementId: this.incrementId,
-            server_url: config.liqpay.server_url
+            server_url: this.liqpayData.server_url
           })
         )
       )
@@ -66,10 +66,13 @@ export default {
       return this.totals.find(it => it.code === 'grand_total').value
     },
 		signature () {
-			return Base64.stringify(sha1(config.liqpay.private_key + this.data + config.liqpay.private_key))
+			return Base64.stringify(sha1(this.liqpayData.private_key + this.data + this.liqpayData.private_key))
     },
     liqpayUpdate () {
       this.liqpayKey += 1
+    },
+    liqpayData () {
+      return this.cartItems.some(it => +it.marketplace) ? config.liqpay_marketplace : config.liqpay
     }
   },
   methods: {
