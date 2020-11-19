25c5882acf11663e2b8ecd60995d639abdb73e60
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2020-11-19 18:07:05 +0200

    Merged PR 20561: fix delivery text
    fix delivery text
    
    Related work items: #218331

--- src/themes/default/components/theme/blocks/Static/Delivery.vue
+++ src/themes/default/components/theme/blocks/Static/Delivery.vue
@@ -15,18 +15,25 @@
     <h2>{{ $t('nova poshta') }}</h2>
     <img src="/assets/static/novaya_poshta.jpg" alt="novaya_poshta">
     <p>{{ $t('nova poshta descrtiption') }}</p>
+    <p>Обрати найближче відділення можна під час оформлення замовлення.</p>
     <h4>{{ $t('cost order') }}</h4>
     <ul>
       <li>{{ $t('nova poshta free order') }}</li>
       <li>{{ $t('nova poshta price order') }}</li>
     </ul>
     <h4>{{ $t('nova poshta postpay') }}</h4>
-    <p>{{ $t('free') }}</p>
-    <h4>{{ $t('delivery time') }}</h4>
     <ul>
-      <li>{{ $t('nova poshta delivery time description') }}</li>
+      <li>Безкоштовно — від 100 грн.</li>
+      <li>Вартість доставки 35 грн — вартість замовлення до 100 грн. (якщо інше не передбачено умовами пропозиції.)</li>
     </ul>
+    <h4>{{ $t('delivery time') }}</h4>
+    <p>Точний час доставки повідомляє наш консультант після підтвердження замовлення.</p>
     <p>{{ $t('delivery warning') }}</p>
+        <h4>Самовивіз з торгової точки</h4>
+    <ul>
+      <li>Безкоштовно</li>
+    </ul>
+    <p>Ви можете оплатити та забрати товар в одному з магазинів ringoo. Дізнатися адресу магазину в своєму місті ви можете у нашого консультанта під час підтвердження замовлення.</p>
     <h2>{{ $t('ukr poshta') }}</h2>
     <img src="/assets/static/ukr_poshta.jpg" alt="ukr_poshta">
     <p>{{ $t('ukr poshta description') }}</p>
@@ -43,6 +50,20 @@ export default {}
 <style lang="scss" scoped>
   .delivery {
     text-align: justify;
+  }
+
+  h4 {
+    font-family: DIN Pro;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 24px;
+    color: #1A1919;
+  }
+
+  p {
+    font-family: DIN Pro;
+    font-size: 15px;
+    line-height: 24px;
   }
 
   h2 {
