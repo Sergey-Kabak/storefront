673244d42dd7c883ba2b09f93e445b10bc5d47e6
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2021-01-14 16:02:37 +0200

    Merged PR 22783: Error pages
    Related work items: #210908

--- src/themes/default/pages/PageNotFound.vue
+++ src/themes/default/pages/PageNotFound.vue
@@ -1,68 +1,55 @@
-<template>
+w<template>
   <div id="page_not_found">
-    <section class="bg-cl-secondary py35 px20">
-      <div class="container">
-        <h2>
-          {{ $t("We can't find the page") }}
-        </h2>
-      </div>
-    </section>
-    <section class="bg-cl-primary py35 px20">
-      <div class="container">
-        <div class="lh16 h5 weight-400">
-          <p>
-            {{ $t("Unfortunately we can't find the page you are looking for.") }}
-          </p>
-          <p>
-            {{ $t('If you need an assistance you can drop us a line on') }}
-            <router-link :to="localizedRoute('/')" class="cl-secondary no-underline">
-              {{ $t('a chat') }}
-            </router-link>
-            {{ $t('or write to us through') }}
-            <router-link :to="localizedRoute('/contact')" class="cl-secondary no-underline">
-              {{ $t('a contact page') }}
-            </router-link>.
-          </p>
-          <p>
-            {{ $t('You can also use') }}
-            <a href="#" class="cl-secondary no-underline" @click="toggleSearchpanel">
-              {{ $t('search') }}
-            </a>
-            {{ $t('to find product you were looking for.') }}
-          </p>
+    <div class="v-container">
+      <div class="error-info">
+        <span class="title"> <span class="highlighted"> 404 </span> - {{ $t('Page not found') }}</span>
+        <span class="info-title">{{ $t('Please check the spelling of the address in the browser bar') }}</span>
+        <div class="emojis">
+          <img src="/assets/emojis/surprised_2.png" alt="surprised" class="emoji">
+          <img src="/assets/emojis/rolling_eyes.png" alt="rolling_eyes" class="emoji">
+          <img src="/assets/emojis/point.png" alt="point" class="emoji">
         </div>
-        <section class="container">
-          <header class="col-md-12 pt40">
-            <h3 class="align-center">
-              {{ $t('See our bestsellers') }}
-            </h3>
-          </header>
-          <lazy-hydrate when-idle>
-            <div class="row center-xs">
-              <div v-for="product in ourBestsellersCollection" :key="product.id" class="col-md-3">
-                <product-tile :product="product" />
-              </div>
-            </div>
-          </lazy-hydrate>
-        </section>
+        <router-link :to="localizedRoute('/')" class="link">
+          <button-full class="not-found">{{ $t('to main') }}</button-full>
+        </router-link>
       </div>
-    </section>
+      <div class="product-info">
+        <span class="product-info-title">{{ $t('Recommend to buy') }}</span>
+        <lazy-hydrate :trigger-hydration="!loading">
+          <product-listing :products="getStockGoods" />
+        </lazy-hydrate>
+      </div>
+      <div class="product-info" v-if="items.length">
+        <span class="product-info-title">{{ $t('Recently viewed') }}</span>
+        <product-listing :products="recentlyViewed" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import { Logger } from '@vue-storefront/core/lib/logger';
+import { isServer } from '@vue-storefront/core/helpers';
+import { mapGetters, mapState } from 'vuex'
 import i18n from '@vue-storefront/i18n';
+import ButtonFull from 'theme/components/theme/ButtonFull';
 import LazyHydrate from 'vue-lazy-hydration';
-import ProductTile from '../components/core/ProductTile.vue';
+import ProductListing from 'theme/components/core/ProductListing';
+import { RecentlyViewedModule } from '@vue-storefront/core/modules/recently-viewed';
+import { registerModule } from '@vue-storefront/core/lib/modules';
 
 export default {
   name: 'PageNotFound',
-  computed: {
-    ...mapGetters({
-      ourBestsellersCollection: 'homepage/getBestsellers'
-    })
+  components: {
+    LazyHydrate,
+    ProductListing,
+    ButtonFull
+  },
+  data: () => ({
+    loading: true
+  }),
+  beforeCreate () {
+    registerModule(RecentlyViewedModule)
   },
   async asyncData ({ store, route, context }) {
     Logger.log('Entering asyncData for PageNotFound ' + new Date())()
@@ -70,29 +57,142 @@ export default {
       context.output.cacheTags.add(`page-not-found`)
       context.server.response.statusCode = 404
     }
-
-    await store.dispatch('homepage/loadBestsellers')
+    await store.dispatch('homepage/loadStockGoods')
+  },
+  beforeRouteEnter (to, from, next) {
+    if (!isServer && !from.name) { // Loading products to cache on SSR render
+      next(vm => {
+        vm.$store.dispatch('homepage/loadStockGoods').then(res => {
+          vm.loading = false
+        })
+      })
+    } else {
+      next()
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getStockGoods: 'homepage/getStockGoods'
+    }),
+    ...mapState('recently-viewed', [
+      'items'
+    ]),
+    recentlyViewed() {
+      return this.items.slice(0, 4)
+    }
   },
   metaInfo () {
     return {
       title: this.$route.meta.title || i18n.t('404 Page Not Found'),
       meta: this.$route.meta.description ? [{ vmid: 'description', name: 'description', content: this.$route.meta.description }] : []
     }
-  },
-  components: {
-    ProductTile,
-    LazyHydrate
-  },
-  methods: {
-    toggleSearchpanel () {
-      this.$store.commit('ui/setSearchpanel', true)
-    }
   }
 }
 </script>
 
-<style scoped>
-  a {
-    text-decoration: underline;
+<style lang="scss" scoped>
+#page_not_found {
+  margin-top: 40px;
+}
+
+.v-container {
+  width: 90%;
+}
+
+.error-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 68px;
+  padding: 0 16px;
+  border-radius: 4px;
+}
+
+.title {
+  font-family: DIN Pro;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+  color: #1A1919;
+  margin-bottom: 16px;
+
+  .highlighted {
+    color: #ee2c39; 
   }
+}
+
+.info-title {
+  font-family: DIN Pro;
+  font-size: 15px;
+  line-height: 24px;
+  color: #5F5E5E;
+  margin-bottom: 16px;
+}
+
+.emojis {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 32px;
+
+  .emoji {
+    margin-right: 12px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+
+.button-full.not-found {
+  max-width: 131px;
+}
+
+.link {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.product-info {
+  margin-bottom: 68px;
+}
+
+.product-info-title {
+  display: block;
+  font-family: DIN Pro;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
+  color: #1A1919;
+  margin-bottom: 32px;
+}
+
+@media (max-width: 768px) {
+  #page_not_found {
+    margin-top: 0px;
+  }
+
+  .error-info {
+    margin-bottom: 48px;
+  }
+
+  .title,
+  .info-title {
+    text-align: center;
+  }
+
+  .button-full.not-found {
+    max-width: 320px;
+  }
+
+  .product-info {
+    margin-bottom: 48px;
+  }
+
+  .product-info-title {
+    margin-bottom: 24px;
+  }
+}
 </style>
