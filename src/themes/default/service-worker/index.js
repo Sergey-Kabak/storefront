c8d6008223427215be26350f063b93bf82547c1f
Підвишенний Тарас Олегович <t.pidvyshennyi@temabit.com>
2021-03-02 10:04:09 +0200

    Merged PR 25996: Performance Hotfix

--- src/themes/default/service-worker/index.js
+++ src/themes/default/service-worker/index.js
@@ -8,18 +8,10 @@ self.addEventListener("activate", event => {
         .then(keyList => {
           return Promise.all(
             keyList.map(key => {
-              console.log('delete ' + cacheName)
+              console.log('delete ' + key)
               return caches.delete(key);
             })
           );
-        })
-        .then(self.clients.claim())
-        .then(async () => {
-          const tabs = await self.clients.matchAll({ type: "window" });
-          console.log("REFRESHING SW IN ALL TABS");
-          tabs.forEach(tab => {
-            tab.navigate(tab.url);
-          });
         })
     )
 });
