d30021c560270b5d46279a9a19412815edf96e4f
Козицький Богдан Сергійович <b.kozytskyi@temabit.com>
2021-04-20 16:00:18 +0300

    Merged PR 29483: hotfix admitad
    hotfix admitad
    
    Related work items: #281933

--- src/themes/default/pages/ThankYouPage.vue
+++ src/themes/default/pages/ThankYouPage.vue
@@ -141,6 +141,7 @@ export default {
       })
       this.status = status.state && status.state.toLowerCase()
     }
+    this.initAdmitad()
     this.initEsputnik()
   },
   methods: {
@@ -153,6 +154,32 @@ export default {
       if (product.original_price > product.price) {
         return parseInt(((product.original_price - product.price) / (product.original_price / 100)))
       }
+    },
+    initAdmitad() {
+      ADMITAD = window.ADMITAD || {};
+      ADMITAD.Invoice = ADMITAD.Invoice || {};
+      ADMITAD.Invoice.category = '1';
+
+      const orderedItem = [];
+      this.products.map(it => {
+        orderedItem.push({
+          Product: {
+            productID: it.item_id,
+            category: '1',
+            price: it.price,
+            priceCurrency: 'UAH',
+          },
+          orderQuantity: it.qty_ordered,
+          additionalType: 'sale'
+        });
+      })
+      ADMITAD.Invoice.referencesOrder = ADMITAD.Invoice.referencesOrder || [];
+
+      ADMITAD.Invoice.referencesOrder.push({
+        orderNumber: this.order.increment_id,
+        orderedItem: orderedItem
+      });
+      ADMITAD.Tracking.processPositions();
     },
     initEsputnik() {
       const items = this.products.map(it => (
