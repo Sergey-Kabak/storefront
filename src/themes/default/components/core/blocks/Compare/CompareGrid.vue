5a78c1900f50625fae3f5e777b350e39502c27cf
sergey.kabak95 <sergey.kabak95@gmail.com>
2021-05-11 17:45:07 +0300

    temp

---
+++ src/themes/default/components/core/blocks/Compare/CompareGrid.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div v-for="(group, key) in data[active]" :key="key">
+      {{key}}
+      <hr>
+      <compare-group :group="group" :product="products[key]"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import CompareGroup from './CompareGroup';
+export default {
+  components: {
+    CompareGroup
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    },
+    products: {
+      type: Array
+    },
+    active: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
---
+++ src/themes/default/components/core/blocks/Compare/CompareGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-for="(attrs, key) in group" :key="key">
+      <h3>{{key}}</h3>
+      <div v-for="(attr, key) in attrs" :key="key">
+        <compare-attribute
+          v-if="attr"
+          :product="product"
+          :attribute="attr"
+          empty-placeholder="N/A"
+        />
+        <div v-else>
+          N/A
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CompareAttribute from './CompareAttribute';
+
+export default {
+  components: {
+    CompareAttribute
+  },
+  props: {
+    group: {
+      type: Object,
+      default: () => {}
+    },
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
